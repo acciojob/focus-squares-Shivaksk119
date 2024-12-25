d0fdf0bb9d62e75a9515de9aa9425a237b0d464e
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-25 23:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll('.square');
+
+squares.forEach((square,index)=>{
+	square.addEventListener('mouseover',()=>{
+		for(let i=0;i<squares.length;i++) {
+			if(i!=index){
+				squares[i].style.backgroundColor = '#6F4E37';
+			}
+		}
+	})
+
+	square.addEventListener('mouseout',()=>{
+		for(let i=0;i<squares.length;i++) {
+			squares[i].style.backgroundColor = '#E6E6FA';
+		}
+	})
+	
+})
